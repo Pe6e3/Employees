e6eb2b4c749ce222f5cb0b9d5bf3262764ee2737
Anton <xpe6e3@gmail.com>
2023-08-20 01:02:58 +0600

    Создал сущности

---
+++ Employees/Entities/Company.cs
@@ -0,0 +1,10 @@
+﻿namespace Employees.Entities
+{
+    internal class Company
+    {
+        public string Name { get; set; }
+        public string INN { get; set; }
+        public string Address { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ Employees/Entities/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Employees.Entities
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string IIN { get; set; }
+    }
+}
