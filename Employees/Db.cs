ebf460270417361108e2487374260c717014fb60
Anton <xpe6e3@gmail.com>
2023-08-20 01:07:25 +0600

    Создал класс Db, прописал связь с БД

---
+++ Employees/Db.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Employees
+{
+    public class Db
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=firstdb");
+
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+
+        }
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+            return connection;
+        }
+
+    }
+}
